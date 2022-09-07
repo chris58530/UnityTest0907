861b72940ecad80e7a8c980b808ff77801e038d8
小仔 <71455438+chris58530@users.noreply.github.com>
2022-09-07 09:20:33 +0800

    change script

--- Unity Project/Assets/Scenes/ll.cs
+++ Unity Project/Assets/Scenes/ll.cs
@@ -7,7 +7,7 @@ public class ll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        yoyoyoyooy;
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
